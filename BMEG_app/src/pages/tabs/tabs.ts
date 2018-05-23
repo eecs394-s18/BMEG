361e94b7a3fb838198d818240bdc158668a80f0d
Mertzstache <ericmertz2018@u.northwestern.edu>
2018-05-23 17:02:19 -0500

    cleaning eslint stuff up

--- BMEG_app/src/pages/tabs/tabs.ts
+++ BMEG_app/src/pages/tabs/tabs.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 
 import { SearchPage } from '../search/search';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-import { ResultsPage } from '../results/results';
 
 @Component({
   templateUrl: 'tabs.html'
