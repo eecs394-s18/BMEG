bbf4e025949acbd278464fe75ae0948c1dd85e35
conwayz <conwayzhu2019@u.northwestern.edu>
2018-05-24 22:16:29 -0500

    word mappings for floor,wall,roof

--- BMEG_app/src/pages/results/results.ts
+++ BMEG_app/src/pages/results/results.ts
@@ -19,10 +19,38 @@ export class ResultsPage {
 	public low_cost:any;
 	public middle:any;
 	public low_impact:any;
+	public floorType:any;
+	public wallType:any;
+	public roofType:any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   	this.low_cost={};
   	this.middle={};
   	this.low_impact={};
+  	this.floorType={
+  		1:'Brick-Concrete',
+  		2:'Stone-Concrete',
+  		3:'Brick-Aerated Conrete'
+  	};
+
+  	this.wallType = {
+  		1:"Brick-Concrete",
+		2:"Stone-Concrete",
+		3:"Bamboo-Concrete",
+		4:"CSEB-Concrete",
+		5:"Concrete Blocks-Concrete",
+		6:"Brick-Aerated Concrete"
+  	};
+
+  	this.roofType = {
+  		1:"Wood-CGI",
+		2:"Wood-Slate",
+		3:"Bamboo-CGI",
+		4:"Wood-Aluminum",
+		5:"Bamboo-Aluminum",
+		6:"Plywood-Wood-Ceramic",
+		7:"Bamboo-Wood-Ceramic"
+  	}
   }
 
   ionViewDidLoad() {
@@ -53,67 +81,67 @@ var data = {
 		1:{
 			'Floor':1,
 			'Walls':2,
-			'Roof':3,
+			'Roof':1,
 			'Cost':4,
 			'Energy':5,
 		},
 		2:{
-			'Floor':6,
-			'Walls':7,
-			'Roof':8,
+			'Floor':2,
+			'Walls':6,
+			'Roof':2,
 			'Cost':9,
 			'Energy':10,
 		},
 		3:{
-			'Floor':11,
-			'Walls':12,
-			'Roof':13,
+			'Floor':3,
+			'Walls':1,
+			'Roof':3,
 			'Cost':14,
 			'Energy':15,
 		},
 	},
 	2:{
 		1:{
-			'Floor':16,
-			'Walls':17,
-			'Roof':18,
+			'Floor':2,
+			'Walls':5,
+			'Roof':4,
 			'Cost':19,
 			'Energy':20,
 		},
 		2:{
-			'Floor':21,
-			'Walls':31,
-			'Roof':41,
+			'Floor':1,
+			'Walls':4,
+			'Roof':5,
 			'Cost':51,
 			'Energy':61,
 		},
 		3:{
-			'Floor':22,
-			'Walls':23,
-			'Roof':24,
+			'Floor':2,
+			'Walls':3,
+			'Roof':6,
 			'Cost':25,
 			'Energy':26,
 		},
 	},
 	3:{
 		1:{
-			'Floor':27,
-			'Walls':28,
-			'Roof':29,
+			'Floor':3,
+			'Walls':2,
+			'Roof':2,
 			'Cost':30,
 			'Energy':31,
 		},
 		2:{
-			'Floor':32,
-			'Walls':33,
-			'Roof':34,
+			'Floor':2,
+			'Walls':6,
+			'Roof':7,
 			'Cost':35,
 			'Energy':36,
 		},
 		3:{
-			'Floor':37,
-			'Walls':38,
-			'Roof':39,
+			'Floor':1,
+			'Walls':4,
+			'Roof':4,
 			'Cost':40,
 			'Energy':41,
 		},
